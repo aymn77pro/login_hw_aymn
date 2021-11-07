cb242567bb595e527355e2c6fa70805ec95be9cc
aymn <aymn.ali.ruh.k@tuwaiq.edu.sa>
2021-11-07 19:20:19 +0300

    Initial commit

---
+++ app/src/main/java/com/example/aymn/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.example.aymn
+
+import android.content.Context
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu,menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+         when(item.itemId){
+            R.id.setting -> {
+                val intent=Intent(this,setting::class.java)
+                startActivity(intent)
+            }
+             R.id.login -> {
+                item.title = "Log out"
+             }
+            else -> super.onOptionsItemSelected(item)
+
+            }
+        return true
+        }
+    }
+
+
+
---
+++ app/src/main/java/com/example/aymn/setting.kt
@@ -0,0 +1,17 @@
+package com.example.aymn
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+
+class setting : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_setting)
+    }
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu,menu)
+        return true
+    }
+
+}
